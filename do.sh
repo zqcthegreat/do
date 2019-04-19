f0b9a777f96a94ac84ffcaee1625af80107b69cc
zqcthegreat <zqc1@qq.com>
2019-04-19 20:18:38 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,4 +1,8 @@
+#curl -sSL https://raw.githubusercontent.com/zqcthegreat/do/master/do.sh | sudo bash
+#curl -sSL https://raw.githubusercontent.com/flyzy2005/ss-fly/master/ss-fly.sh | sudo bash
 service crond start
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
 echo "0 1 * * * root ntpdate 0.asia.pool.ntp.org" >> /etc/crontab
 echo "0 4 * * * root yum -y  update" >> /etc/crontab
+
+
