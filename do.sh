72e09300b18be7e696dd5489e3b4692d976e3028
zqcthegreat <zqc1@qq.com>
2019-04-19 20:29:41 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -5,4 +5,7 @@ cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "0 1 * * * root ntpdate 0.asia.pool.ntp.org" >> /etc/crontab
 echo "0 4 * * * root yum -y  update" >> /etc/crontab
 
+yum -y install git
+git clone https://github.com/flyzy2005/ss-fly
+ss-fly/ss-fly.sh -i good666 6666
 
