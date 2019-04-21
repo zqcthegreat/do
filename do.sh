cfc098c60a77b2973fe6c57ba120c2af101a84fb
zqcthegreat <zqc1@qq.com>
2019-04-22 02:01:37 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,11 +1,6 @@
-#curl -sSL https://raw.githubusercontent.com/zqcthegreat/do/master/do.sh | sudo bash
-#curl -sSL https://raw.githubusercontent.com/flyzy2005/ss-fly/master/ss-fly.sh | sudo bash
-service crond start
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
-echo "0 1 * * * root ntpdate 0.asia.pool.ntp.org" >> /etc/crontab
-echo "0 4 * * * root yum -y  update" >> /etc/crontab
+ntpdate 0.asia.pool.ntp.org
 
 yum -y install git
 git clone https://github.com/flyzy2005/ss-fly
 ss-fly/ss-fly.sh -i good666 6666
-
