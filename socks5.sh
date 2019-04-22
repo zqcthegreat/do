158fc3593b0ed0b70e138ffc1218395056004eac
zqcthegreat <zqc1@qq.com>
2019-04-22 10:26:41 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,4 +1,3 @@
 #https://blog.csdn.net/yoie01/article/details/84973311
-yum install wget -y
 wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh
 bash install.sh  --port=6666 --user=dalao --passwd=good6666
